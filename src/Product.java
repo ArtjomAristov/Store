c85fda9adaf6831ee4f3c57d2d9ba5bef5d6a427
ArtjomAristov <131642348+ArtjomAristov@users.noreply.github.com>
2023-11-14 21:38:55 +0200

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,10 +1,12 @@
 public class Product {
+    private int amount;
     private String name;
     private double price;
 
-    public Product(String name, double price) {
+    public Product(String name, double price, int amount) {
         this.name = name;
         this.price = price;
+        this.amount = amount;
     }
 
     public String getName() {
@@ -13,6 +15,18 @@ public class Product {
 
     public double getPrice() {
         return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public boolean changeAmount() {
+        if (amount > 0) {
+            amount--;
+            return true;
+        }
+        return false;
     }
 }
 
